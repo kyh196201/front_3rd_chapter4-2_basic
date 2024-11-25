c5c34bf9a0f31770e4ad2853af05e5423c9e0281
Seungwoo Kim <kyh196201@gmail.com>
2024-11-26 01:10:20 +0900

    feat: 상품 이미지 레이지 로드

--- js/products.js
+++ js/products.js
@@ -22,7 +22,8 @@ function displayProducts(products) {
         const img = document.createElement('img');
         img.src = product.image;
         img.alt = `product: ${product.title}`;
-        img.width=250;
+        img.width = 250;
+        img.loading = 'lazy';
         pictureDiv.appendChild(img);
 
         // Create the product info div
